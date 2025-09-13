9d150988e82491a04c97e1f4d27ad948add79588
JI1047 <kjs0010046@gmail.com>
2025-09-13 14:31:27 +0900

    코드 리뷰 받은 내용 수정

--- src/main/java/org/example/DigitalWatch.java
+++ src/main/java/org/example/DigitalWatch.java
@@ -3,6 +3,7 @@ package org.example;
 import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 
 public class DigitalWatch extends SunWatch{
@@ -14,59 +15,17 @@ public class DigitalWatch extends SunWatch{
     @Override
     public void showTime() {
         LocalTime now = LocalTime.now();
-
-
-        //현재 hour
-        int hour = now.getHour();
-
-        //현재 minute
-        int minute = now.getMinute();
-
-        //현재 second
-        int second = now.getSecond();
-
-        System.out.print("현재 시간은 ");
-        //hour,minute,second가 한자리 수일 때 앞자리에 0을 채워 전자식으로 표현
-        if(hour<10){
-            String formatHour = String.format("%02d", hour);
-            System.out.print(formatHour);
-        }
-        else{
-            System.out.print(hour);
-        }
-        if(minute<10){
-            String formatMin = String.format("%02d", minute);
-            System.out.print(":"+ formatMin);
-        }
-        else{
-            System.out.print(":"+minute);
-        }
-        if(second<10){
-            String formatSec = String.format("%02d", second);
-            System.out.println(":"+ formatSec+" 입니다");
-        }
-        else{
-            System.out.println(":"+second+" 입니다");
-        }
-
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        System.out.println("현재 시간은 " + now.format(formatter) + " 입니다");
     }
 
     //년,월,일을 보여주는 메서드
     public void showYMD() {
 
-        Calendar calendar = Calendar.getInstance();
-
-        //오늘 날짜에 해당하는 year
-        int year = calendar.get(Calendar.YEAR);
-
-        //오늘 날짜에 해당하는 month
-        int month = calendar.get(Calendar.MONTH) + 1;// 0 부터 시작하기 때문에 1을 더함
-
-        //오늘 날짜에 해당하는 day
-        int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-
-        System.out.println("오늘은 "+year+"년 "+month+"월 "+day+"일입니다.");
+        LocalDate today = LocalDate.now();
+        System.out.println("오늘은 "+today.getYear()+"년 "
+                                    +today.getMonthValue() + "월 "
+                                    +today.getDayOfMonth() + "일입니다.");
     }
 
 
